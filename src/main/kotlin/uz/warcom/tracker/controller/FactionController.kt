c6f74ddc858420ada6249145046561c7c6fc0693
temur.navoi <temur.navoi@yandex.com>
2021-11-06 23:21:18 +0100

    feat: add FactionController api description

--- src/main/kotlin/uz/warcom/tracker/controller/FactionController.kt
+++ src/main/kotlin/uz/warcom/tracker/controller/FactionController.kt
@@ -1,12 +1,18 @@
 package uz.warcom.tracker.controller
 
+import io.swagger.v3.oas.annotations.Operation
+import io.swagger.v3.oas.annotations.tags.Tag
 import uz.warcom.tracker.api.tracker.model.request.FactionRequest
 import uz.warcom.tracker.api.tracker.model.response.FactionResponse
 
+@Tag(name="FactionApi", description = "management of factions")
 interface FactionController {
-
+    @Operation(summary = "create new faction")
     fun createFaction (request: FactionRequest): FactionResponse
+    @Operation(summary = "update existing faction")
     fun updateFaction (request: FactionRequest): FactionResponse
+    @Operation(summary = "get existing faction")
     fun getFaction (code: String): FactionResponse
+    @Operation(summary = "get list of all factions")
     fun getFactions (): List<FactionResponse>
 }
