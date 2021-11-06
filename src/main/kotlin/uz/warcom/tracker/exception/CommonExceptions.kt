547102f71d7a4ab830a8ef78af4a3103cad7b1c8
temur.navoi <temur.navoi@yandex.com>
2021-11-06 23:23:58 +0100

    feat: add defailt http error codes for custom exceptions

--- src/main/kotlin/uz/warcom/tracker/exception/CommonExceptions.kt
+++ src/main/kotlin/uz/warcom/tracker/exception/CommonExceptions.kt
@@ -1,13 +1,17 @@
 package uz.warcom.tracker.exception
 
+import org.springframework.http.HttpStatus
+import org.springframework.web.bind.annotation.ResponseStatus
 import java.util.*
 import kotlin.reflect.KClass
 
+@ResponseStatus(HttpStatus.BAD_REQUEST)
 open class GenericApiException(
     val code: String = "",
     override val message: String = ""
 ): RuntimeException(message)
 
+@ResponseStatus(HttpStatus.NOT_FOUND)
 class ResourceNotFoundException(
     override val message: String = "Resource not found"
 ): GenericApiException("NOT_FOUND", message) {
