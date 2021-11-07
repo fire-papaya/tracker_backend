678124930b599bc54dba21a6a20eb0b8ddaefad4
temur.navoi <temur.navoi@yandex.com>
2021-11-07 18:50:23 +0100

    feat: change parent representation in FactionResponse to string

--- src/main/kotlin/uz/warcom/tracker/api/tracker/configuration/MapStructConfiguration.kt
+++ src/main/kotlin/uz/warcom/tracker/api/tracker/configuration/MapStructConfiguration.kt
@@ -1,6 +1,8 @@
 package uz.warcom.tracker.api.tracker.configuration
 
 import org.mapstruct.Mapper
+import org.mapstruct.Mapping
+import org.mapstruct.Mappings
 import org.springframework.context.annotation.Configuration
 import uz.warcom.tracker.api.tracker.model.request.FactionRequest
 import uz.warcom.tracker.api.tracker.model.response.FactionResponse
@@ -10,6 +12,9 @@ import uz.warcom.tracker.domain.entity.Faction
 @Mapper(componentModel = "spring")
 interface ApiMapStructConverter {
 
+    @Mappings(value = [
+        Mapping(target = "parent", source = "parent.code")
+    ])
     fun toFactionResponse (entity: Faction): FactionResponse
 
     fun toFactionEntity (request: FactionRequest): Faction
--- src/main/kotlin/uz/warcom/tracker/api/tracker/model/response/FactionResponse.kt
+++ src/main/kotlin/uz/warcom/tracker/api/tracker/model/response/FactionResponse.kt
@@ -3,6 +3,6 @@ package uz.warcom.tracker.api.tracker.model.response
 data class FactionResponse (
     var name: String = "",
     var code: String = "",
-    var parent: FactionResponse? = null,
+    var parent: String? = null,
     var children: List<FactionResponse>? = null
 )
